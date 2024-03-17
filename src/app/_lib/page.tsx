3c140fb948dc988776af4f72c4a727a8ff260db9
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-03-16 19:08:40 -0600

    private folder
    private folder

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivatePage() {
+    return (
+      <>
+        <h2>Private Lib page.tsx. This page should not render and return 404</h2>
+      </>
+    );
+  }
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 import Batch from "@/components/Batch";
+import { toUpper } from "../_lib/case";
 
 export default function About() {
   return (
     <>
       <h2>About Page</h2>
       <Batch />
+      {toUpper('This text from Private folder with lib toUpper')}
     </>
   );
 }
