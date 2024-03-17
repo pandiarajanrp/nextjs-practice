8012480df5ce9f89d7dc1b261a97ac19347ad429
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-03-16 19:01:46 -0600

    file colocation
    file colocation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,10 @@
+import Batch from "@/components/Batch";
+
 export default function About() {
-    return <h2>About Page</h2>
+  return (
+    <>
+      <h2>About Page</h2>
+      <Batch />
+    </>
+  );
 }
