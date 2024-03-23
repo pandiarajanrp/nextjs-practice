9a139cea2a19afec2ed231669a7a775c80be034a
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-03-22 18:02:55 -0600

    redirect
    redirect

--- src/app/comments/api/[id]/route.ts
+++ src/app/comments/api/[id]/route.ts
@@ -1,8 +1,12 @@
 import { comments } from '../../data/mock';
+import { redirect } from "next/navigation";
 
 export async function GET(request: Request, context: any) {
     const { id } = context.params
     const comment = comments.find(item => item.id === id);
+    if (!comment) {
+        redirect('/comments/api')
+    }
     console.log('***** comment', comment)
     return Response.json(comment);
 }
