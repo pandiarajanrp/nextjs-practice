937f94aa61b16c51f60ad8165cb5817c9d196a00
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-03-22 18:24:10 -0600

    request and res headers
    request and res headers

--- src/app/comments/api/route.ts
+++ src/app/comments/api/route.ts
@@ -1,25 +1,29 @@
 import { type NextRequest } from 'next/server.js';
+import { headers } from 'next/headers';
 import { comments } from '../data/mock';
 
 const sendJson = (data: any, status?: number) => {
-    return new Response(JSON.stringify(data), {
-        status: status || 200,
-        headers: {
-            "content-type": "application/json"
-        }
-    })
+  return new Response(JSON.stringify(data), {
+    status: status || 200,
+    headers: {
+      "content-type": "application/json"
+    }
+  })
 }
 
 export async function GET(request: NextRequest) {
-    const { searchParams } = request.nextUrl
-    //get query params from url
-    const query = searchParams.get('query');
-    return sendJson(comments)
+  const { searchParams } = request.nextUrl
+  //get query params from url
+  const query = searchParams.get('query');
+  //get headers
+  const reqHeaders = headers();
+  console.log('**** reqHeader', reqHeaders.get('authorization'))
+  return sendJson(comments)
 }
 
 
 export async function POST(req: any) {
-    const data = await req.json()
-    comments.push(data);
-    return sendJson({ status: true, message: 'Created Successfully' })
+  const data = await req.json()
+  comments.push(data);
+  return sendJson({ status: true, message: 'Created Successfully' })
 }
